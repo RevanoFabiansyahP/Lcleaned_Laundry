ef5ecc0311094b3b2d98dbbe48ac5f71b9d86a45
Your Name <you@example.com>
2022-07-29 00:01:16 +0700

    First Commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,29 @@
+@extends('admin.index')
+
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <!-- /.card-header -->
+              <div class="card-body">
+              	<p><h2>Selamat Datang !!!</h2></p>
+              	<p><h3>Admin LCleaned</h3></p>
+              	
+              </div>
+              
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
---
+++ resources/views/emails/forgot-password.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+Anda telah melakukan permintaan reset password,
+silakan klik link ini untuk reset password
+<button type="button" style="color: red;"><a href="{{ url('/') }}/reset-password">Reset Password</a></button>
+
+</body>
+</html>
